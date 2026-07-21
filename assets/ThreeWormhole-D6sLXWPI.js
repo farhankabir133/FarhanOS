1a824d4249ada22997bb5961d8706f0e3cde854c
farhankabir133 <farhankabir133@gmail.com>
2026-07-21 13:31:41 +0600

    Updates

--- assets/ThreeWormhole-D6sLXWPI.js
+++ assets/ThreeWormhole-D6sLXWPI.js
@@ -1,4 +1,4 @@
-import{r as ji,j as ro}from"./index-rqLTmb-c.js";/**
+import{r as ji,j as ro}from"./index-317pIq0K.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
