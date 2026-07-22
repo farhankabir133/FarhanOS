71fa562558b23120266ddd8857fbfbf87ba7d137
farhankabir133 <farhankabir133@gmail.com>
2026-07-22 07:43:37 +0600

    Updates

--- assets/ThreeWormhole-rISli-KH.js
+++ assets/ThreeWormhole-rISli-KH.js
@@ -1,4 +1,4 @@
-import{r as ji,j as ro}from"./index-BPxffnFe.js";/**
+import{r as ji,j as ro}from"./index-CdQcKXb_.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
